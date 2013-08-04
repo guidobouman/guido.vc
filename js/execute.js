debc4bf30fc421efdab346c8bf34748f2b10750c
Guido Bouman <m@guido.vc>
2013-08-04 16:01:02 +0400

    Latest version of guido.vc before using git.

--- js/execute.js
+++ js/execute.js
@@ -1,75 +1,104 @@
 jQuery(function($)
 {
-	$('section.home .first_name').fitText(0.33, { maxFontSize: '120px' });
-	$('section.home .last_name').fitText(0.44, { maxFontSize: '120px' });
-	$('section.work h1').fitText(0.30, { maxFontSize: '120px' });
-	$('section.contact h1').fitText(0.475, { maxFontSize: '120px' });
-	
-	$('<div id="highlight"/>').appendTo('body');
-	
-	$('header a').not('.no_highlight').hover(
-		function(e)
-		{
-			moveHighlight($(this));
-		},
-		function()
-		{
-			moveHighlight($('a.active'));
-		}
-	).focusin(function()
-	{
-		moveHighlight($(this));
-	}).focusout(function()
-	{
-		moveHighlight($('a.active'));
-	}).click(function()
-	{
-		$('a.active').removeClass('active');
-		$(this).addClass('active');
-		
-		var target = $('section.' + $.trim($(this).text()));
-		switchViewport(target);
-		
-		return false;
-	});
-	
-	// Crappy FOUT rendering...
-	setTimeout(function()
-	{
-		moveHighlight($('a.active'), 0);
-		$('#highlight').animate({opacity: 1},{queue: false, duration: 500});
-	}, 150);
-	
+  $(window).resize(function()
+  {
+    if($(this).width() < 750)
+    {
+      narrow = true;
+    }
+    else
+    {
+      narrow = false;
+    }
+
+    init(narrow);
+  }).resize();
+
+  /mobi/i.test(navigator.userAgent) && !location.hash && setTimeout(function () {
+    if (!pageYOffset) window.scrollTo(0, 0);
+  }, 100);
 });
 
-var moveHighlight = function(target, speed)
+var init = function(narrow)
 {
-	if(typeof(speed) == 'undefined')
-	{
-		speed = 500;
-	}
-	
-	var scrollTop = $('body').scrollTop();
-	var offset = target.offset();
-	var width = target.width();
-	var height = target.height();
-	
-	$('#highlight').stop('fx', true).animate(
-	{
-		top: offset.top - scrollTop + height,
-		left: offset.left,
-		width: width
-	}, speed);
+  if(!window.initialised)
+  {
+    window.initialised = true;
+    $('section.home .first_name').fitText(0.33, { maxFontSize: '120px' });
+    $('section.home .last_name').fitText(0.44, { maxFontSize: '120px' });
+    $('section.home h2').fitText(1.45, { maxFontSize: '24px' });
+    $('section.work h1').fitText(0.30, { maxFontSize: '120px' });
+    $('section.contact h1').fitText(0.475, { maxFontSize: '120px' });
+  }
+
+  if(narrow && window.wide_initialised)
+  {
+    window.wide_initialised = false;
+
+    $('#highlight').remove();
+
+    $('body').panelSnap('destroy');
+
+    clearTimeout(window.delayedInit);
+  }
+  else if(!narrow && !window.wide_initialised)
+  {
+    window.wide_initialised = true;
+
+
+    $('body').panelSnap(
+    {
+      $menu: $('header'),
+      onSnapFinish: function()
+      {
+        var self = this;
+
+        moveHighlight($('a.active'));
+      }
+    });
+
+    $('<div id="highlight"/>').appendTo('body');
+
+    $('header a').not('.no_highlight').on('mouseenter focusin', function(e)
+    {
+      moveHighlight($(this));
+    }).on('mouseleave focusout', function(e)
+    {
+      moveHighlight($('a.active'));
+    });
+
+    // Crappy FOUT rendering...
+    window.delayedInit = setTimeout(function()
+    {
+      // Init highlightSnap
+      // Invoke scrollStop
+
+      moveHighlight($('a.active'), 0);
+      $('#highlight').animate({opacity: 1},{queue: false, duration: 500});
+
+      $(document).trigger('scrollstop');
+    }, 500);
+  }
 }
 
-var switchViewport = function(target, speed)
+var moveHighlight = function($target, speed)
 {
-	if(typeof(speed) == 'undefined')
-	{
-		speed = 500;
-	}
-	
-	$('body').animate({
-		scrollTop: target.offset().top
-	}, speed);
+  if(typeof(speed) == 'undefined')
+  {
+    speed = 500;
+  }
+
+  var $body = $('body');
+  var canvasWidth = $body.width();
+  var scrollTop = $body.scrollTop();
+  var offset = $target.offset();
+  var width = $target.width();
+  var height = $target.height();
+
+  $('#highlight').stop('fx', true).animate(
+  {
+    top: offset.top - scrollTop + height,
+    right: canvasWidth - width - offset.left,
+    width: width
+  }, speed);
 }
