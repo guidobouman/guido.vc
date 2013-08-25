122dcc38e766cc0d2f6100af5256dffd840bed7d
Guido Bouman <m@guido.vc>
2013-08-25 17:41:11 +0200

    Improved work & contact sections.

--- js/execute.js
+++ js/execute.js
@@ -1,18 +1,47 @@
-jQuery(function($)
-{
-  $(window).resize(function()
-  {
-    if($(this).width() < 750)
-    {
+jQuery(function($) {
+
+  $(window).resize(function() {
+    if($(this).width() < 750) {
       narrow = true;
-    }
-    else
-    {
+    } else {
       narrow = false;
     }
 
     init(narrow);
   }).resize();
+
+  $('a.phone').on('click', function(e) {
+    e.preventDefault();
+
+    $('.modal section').hide();
+    $('.modal section.phone').show();
+    $('.modal').fadeIn();
+  });
+
+  $('a.close').on('click', function(e) {
+    e.preventDefault();
+
+    $('.modal').fadeOut();
+  });
+
+  $('a.mail').on('click', function(e) {
+    e.preventDefault();
+
+    var mail = '';
+    mail += 'm@';
+    mail += 'guido';
+    mail += '.vc';
+    // document.location.href = 'mailto:' + mail;
+
+    $('.modal section').hide();
+    $('.modal section.mail').show();
+    $('.modal section.mail h1').html(mail);
+    $('.modal').fadeIn();
+  });
+
+  $('.modal section.mail h1').on('click' , function() {
+    document.location.href = 'mailto:' + $(this).html();
+  })
 
   /mobi/i.test(navigator.userAgent) && !location.hash && setTimeout(function () {
     if (!pageYOffset) window.scrollTo(0, 0);
@@ -28,21 +57,6 @@ var init = function(narrow)
     $('section.home h2').fitText(1.70, { maxFontSize: '24px' });
     $('section.work h1').fitText(0.30, { maxFontSize: '120px' });
     $('section.contact h1').fitText(0.475, { maxFontSize: '120px' });
-  }
-
-  if(narrow && window.wide_initialised)
-  {
-    window.wide_initialised = false;
-
-    $('#highlight').remove();
-
-    $('body').panelSnap('destroy');
-
-    clearTimeout(window.delayedInit);
-  }
-  else if(!narrow && !window.wide_initialised)
-  {
-    window.wide_initialised = true;
 
 
     $('body').panelSnap(
@@ -55,6 +69,19 @@ var init = function(narrow)
         moveHighlight($('a.active'));
       }
     });
+  }
+
+  if(narrow && window.wide_initialised)
+  {
+    window.wide_initialised = false;
+
+    $('#highlight').remove();
+
+    clearTimeout(window.delayedInit);
+  }
+  else if(!narrow && !window.wide_initialised)
+  {
+    window.wide_initialised = true;
 
     $('<div id="highlight"/>').appendTo('body');
 
